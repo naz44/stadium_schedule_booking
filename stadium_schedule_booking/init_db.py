2a9a449bd2e39f437a1ffdf9e556a182e38711d1
Nazish zaha <53830043+naz44@users.noreply.github.com>
2021-12-30 13:52:54 +0530

    Update init_db.py

--- stadium_schedule_booking/init_db.py
+++ stadium_schedule_booking/init_db.py
@@ -11,7 +11,7 @@ cur = connection.cursor()
 cur.execute("INSERT INTO users (username,emailid,password,firstname,lastname,favsports,admin) VALUES (?,?,?,?,?,?,?)", 
 ('admin1','alzaha2427@gmail.com','admin@123','Nz','sk',null,'yes'))
 
-cur.execute("INSERT INTO admin (username,emailid,password,firstname,lastname,favsports,admin) VALUES (?,?,?,?,?,?,?)",('admin2''jjtjasonthomas@gmail.com','admin@456','J','T',null,'yes'))
+cur.execute("INSERT INTO users (username,emailid,password,firstname,lastname,favsports,admin) VALUES (?,?,?,?,?,?,?)",('admin2''jjtjasonthomas@gmail.com','admin@456','J','T',null,'yes'))
 
 
 
