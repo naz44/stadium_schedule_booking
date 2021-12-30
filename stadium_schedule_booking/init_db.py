d12334f429ae27ada20cbb2403f31847c4a5cd80
Nazish zaha <53830043+naz44@users.noreply.github.com>
2021-12-30 14:17:06 +0530

    Update init_db.py

--- stadium_schedule_booking/init_db.py
+++ stadium_schedule_booking/init_db.py
@@ -9,9 +9,10 @@ with open('schema.sql') as f:
 cur = connection.cursor()
 
 cur.execute("INSERT INTO users (username,emailid,password,firstname,lastname,favsports,admin) VALUES (?,?,?,?,?,?,?)", 
-('admin1','alzaha2427@gmail.com','admin@123','Nz','sk','random','yes'))
+('admin1','alzaha2427@gmail.com','admin@123','Nz','sk',None,'yes'))
 
-cur.execute("INSERT INTO users (username,emailid,password,firstname,lastname,favsports,admin) VALUES (?,?,?,?,?,?,?)",('admin2','jjtjasonthomas@gmail.com','admin@456','J','T','random','yes'))
+cur.execute("INSERT INTO users (username,emailid,password,firstname,lastname,favsports,admin) VALUES (?,?,?,?,?,?,?)", 
+('admin2','jjtjasonthomas@gmail.com','admin@456','J','T',None,'yes'))
 
 
 connection.commit()
